339ae40b0209752dea975124f5e3e23fec17049b
nathan-assefa <aasamnew@gmail.com>
2023-06-15 06:20:48 +0000

    AirBnB_clone_v3

--- web_dynamic/static/scripts/2-hbnb.js
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -18,7 +18,7 @@ $(document).ready(function() {
 });
 
 $(document).ready(function() {
-    $.get('http://0.0.0.0:5001/api/v1/status/', function(data) {
+    $.get('http://54.237.108.7/api/v1/status/', function(data) {
         if (data.status === 'OK') {
             $('#api_status').addClass('available');
         } else {
