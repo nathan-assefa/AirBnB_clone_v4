a416145ded3c97d203482d4dd5e68ab3813dc6c8
nathan-assefa <aasamnew@gmail.com>
2023-06-24 11:18:21 +0000

    AirBnB_clone_v3

--- web_dynamic/3-hbnb.py
+++ web_dynamic/3-hbnb.py
@@ -2,10 +2,9 @@
 """ This script lets the flask app connects to mysql database and
 fetch all the data from the states table.
 
-ip address 0.0.0.0 is going to be used to all the machines within the
-the network to have access to our app. Port 5000 will be used at entry
+ip address 0.0.0.0 is used to allow all the network interface ip
+addresses to have access to our app. Port 5000 will be used at entry
 point """
-
 
 from flask import Flask, render_template
 from models import storage
@@ -18,7 +17,7 @@ app = Flask(__name__)
 
 
 @app.teardown_appcontext
-def close_dp(exit):
+def close_db(exit):
     """This context function gives back the
     connection once request is done"""
     storage.close()
